5a09509fb81f0daf88633399ebf9e436e2204822
Ronaldo <ronaldoon@gmail.com>
2020-08-29 17:57:14 -0300

    29/08

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:doglife/pages/login.page.dart';
-import 'package:doglife/pages/reset-password.page.dart';
+//import 'package:doglife/pages/reset-password.page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
--- lib/pages/login.page.dart
+++ lib/pages/login.page.dart
@@ -1,6 +1,5 @@
-import 'package:doglife/pages/reset-password.page.dart';
-import 'package:doglife/pages/home.page.dart';
-import 'package:doglife/pages/signup.page.dart';
+
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -8,182 +7,72 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.only(top: 60, left: 40, right: 40),
-        color: Colors.white,
-        child: ListView(
-          children: <Widget>[
-            SizedBox(
-              width: 128,
-              height: 128,
-              child: Image.asset("assets/logo.png"),
-            ),
+        padding: EdgeInsets.only(
+          top: 60,
+          left: 40,
+          right: 40,
+        ),
+
+        color: Colors.black,
+
+         child: ListView(
+            children:<Widget> [
+              SizedBox(
+                width: 128,
+                height: 128,
+                child: Image.asset("assets/logo.png"),
+              ),
             SizedBox(
               height: 20,
             ),
             TextFormField(
-              // autofocus: true,
+              autofocus: true,
               keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                labelText: "E-mail",
+               decoration: InputDecoration(
+                 focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Color.fromRGBO(148, 0, 211, 10,))),
+                fillColor: Colors.amber,
+                labelText: "Email:",
                 labelStyle: TextStyle(
-                  color: Colors.black38,
-                  fontWeight: FontWeight.w400,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
                   fontSize: 20,
+                  ),
                 ),
-              ),
-              style: TextStyle(fontSize: 20),
+              
+              style: TextStyle(
+                fontSize: 15,
+                color: Colors.white,
+                ),
             ),
             SizedBox(
               height: 10,
             ),
             TextFormField(
-              // autofocus: true,
+              
+              autofocus: true,
               keyboardType: TextInputType.text,
               obscureText: true,
               decoration: InputDecoration(
-                labelText: "Senha",
+              focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Color.fromRGBO(148, 0, 211, 10,))),  
+                labelText: "Senha:",
                 labelStyle: TextStyle(
-                  color: Colors.black38,
-                  fontWeight: FontWeight.w400,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w100,
                   fontSize: 20,
-                ),
+                  
+                  )
               ),
-              style: TextStyle(fontSize: 20),
-            ),
-            Container(
-              height: 40,
-              alignment: Alignment.centerRight,
-              child: FlatButton(
-                child: Text(
-                  "Recuperar Senha",
-                  textAlign: TextAlign.right,
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ResetPasswordPage(),
-                    ),
-                  );
-                },
-              ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Container(
-              height: 60,
-              alignment: Alignment.centerLeft,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  stops: [0.3, 1],
-                  colors: [
-                    Color(0xFFF58524),
-                    Color(0XFFF92B7F),
-                  ],
-                ),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(5),
-                ),
-              ),
-              child: SizedBox.expand(
-                child: FlatButton(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        "Login",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
-                        textAlign: TextAlign.left,
-                      ),
-                      Container(
-                        child: SizedBox(
-                          child: Image.asset("assets/bone.png"),
-                          height: 28,
-                          width: 28,
-                        ),
-                      )
-                    ],
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => HomePage(),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: 60,
-              alignment: Alignment.centerLeft,
-              decoration: BoxDecoration(
-                color: Color(0xFF3C5A99),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(5),
-                ),
-              ),
-              child: SizedBox.expand(
-                child: FlatButton(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        "Login com Facebook",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
-                        textAlign: TextAlign.left,
-                      ),
-                      Container(
-                        child: SizedBox(
-                          child: Image.asset("assets/fb-icon.png"),
-                          height: 28,
-                          width: 28,
-                        ),
-                      )
-                    ],
-                  ),
-                  onPressed: () {},
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: 40,
-              child: FlatButton(
-                child: Text(
-                  "Cadastre-se",
-                  textAlign: TextAlign.center,
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => SignupPage(),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
+              style: TextStyle(
+	      fontSize: 20,
+        color: Colors.white,
         ),
+        ),
+
+            ],
+         )
+        
       ),
+
     );
   }
 }
